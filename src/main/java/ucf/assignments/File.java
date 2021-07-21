29c4cf20d16c3d895efb650510c43285f1355235
Brandon Knudson <bknudson117@gmail.com>
2021-07-21 18:06:03 -0400

    Commit 2

---
+++ src/main/java/ucf/assignments/File.java
@@ -0,0 +1,28 @@
+package ucf.assignments;
+
+public class File {
+    static public void createTSV() {
+
+    }
+    static public void createHTML() {
+
+    }
+    static public void createJSON() {
+
+    }
+    static public void readTSV() {
+
+    }
+    static public void readHTML() {
+
+    }
+    static public void readJSON() {
+
+    }
+    static public void openFile() {
+
+    }
+    static public void closeFile() {
+
+    }
+}
